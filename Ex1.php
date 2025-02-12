f8a0773d1af8f39c0c1a8ccd998f226bfe04d621
Jolann Lemouton <jojorauline@gmail.com>
2025-02-12 18:19:23 +0100

    modif Ex1

--- Ex1.php
+++ Ex1.php
@@ -6,5 +6,11 @@
 </head>
 <body>
  <h1>Mon fichier HTML</h1>
+ <?php 
+        echo "<p>On est le";
+        echo date(D-M-Y);
+        echo "</p>";
+        echo"<p>Je test git et Vscode</p>";
+    ?>
 </body>
 </html>
